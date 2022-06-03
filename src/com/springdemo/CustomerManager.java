3e5725b43ace2c350790fabe86e3261c6f42980b
Ramazan AKSOY <68793314+rmznaksy@users.noreply.github.com>
2022-06-03 11:54:34 +0300

    Add files via upload

---
+++ src/com/springdemo/CustomerManager.java
@@ -0,0 +1,20 @@
+package com.springdemo;
+
+public class CustomerManager implements ICustomerService {
+	
+	private ICustomerDal customerDal;
+	
+	//constructor injection
+	public CustomerManager(ICustomerDal customerDal) {
+		this.customerDal = customerDal;
+	}
+
+
+	public void add() {
+		//iþ kurallarý
+		
+		customerDal.add();
+		
+	}
+
+}
